5fcf4b038d11a875455016c95c41c8127aad31b2
vboi <lukasbaier@outlook.de>
2020-05-16 15:27:41 +0200

    roomtemp.png

---
+++ autoscript.py
@@ -0,0 +1,14 @@
+from picamera import PiCamera
+import time
+import os
+
+with PiCamera() as camera:
+	camera.start_preview()
+	while True:
+		print("New cycle, please wait...")
+		camera.capture("roomtemp.png")
+		os.system("git add .")
+		os.system("git commit -m 'roomtemp.png'")
+		os.system("git push origin vboi.github.io")
+		time.sleep(60)
+
