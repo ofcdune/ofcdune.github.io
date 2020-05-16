5bdc3dada474522eec22e31bc91da3d199ba2b97
vboi <lukasbaier@outlook.de>
2020-05-16 17:00:54 +0200

    Update autoscript.py

--- autoscript.py
+++ autoscript.py
@@ -2,12 +2,11 @@ from picamera import PiCamera
 import time
 import os
 
+os.system("git pull")
 with PiCamera() as camera:
-	camera.start_preview()
 	while True:
 		print("New cycle, please wait...")
 		camera.capture("roomtemp.png")
-		os.system("git pull")
 		os.system("git add .")
 		os.system("git commit -m 'roomtemp.png'")
 		os.system("git push origin")
