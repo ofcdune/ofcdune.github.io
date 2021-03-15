7a6a46fa2bc53edc30625ef5ae42c4c9c5a55c4c
dune-official <lukasbaier@outlook.de>
2021-03-15 09:07:43 +0100

    Update script.js
    The script now updates the new website, not by calculating days etc but by taking the string calculated by python and sent to the api

--- script.js
+++ script.js
@@ -1,30 +1,23 @@
 async function getData () {
   let response = await fetch('https://server.vboi.repl.co/countdowns/all').then(response => response.json());
+  // Gather information about the respective countdown
   let time = response[0].seconds_left;
   let name = response[0].name;
+  let timestring = response[0].string;
+
+  let resulting_Date = new Date(time * 1000)
+  modifyElements(name, time, timestring);
   
+}
+
+function modifyElements(name, total, timestr) {
   let heading = document.getElementById("headline");
   heading.textContent = name + ' Countdown';
   
-  let resulting_Date = new Date(time * 1000)
-  //modifyElements(resulting_Date.getDay(), resulting_Date.getHours(), resulting_Date.getMinutes(), resulting_Date.getSeconds(), time)
-}
-
-/*
-function modifyElements(days, hours, minutes, seconds, total) {
-  let daysdiv = document.getElementById("days");
-  daysdiv.textContent = days;
-  
-  let hoursdiv = document.getElementById("hours");
-  hoursdiv.textContent = hours;
-  
-  let minutessdiv = document.getElementById("minutes");
-  minutessdiv.textContent = minutes;
-  
-  let secondsdiv = document.getElementById("seconds");
-  secondsdiv.textContent = seconds;
-  
   let unixdiv = document.getElementById("unix");
   unixdiv.textContent = total;
+  
+  let timestringdiv = document.getElementById("datetime_string");
+  timestringdiv.textContent = timestr;
 }
 */
