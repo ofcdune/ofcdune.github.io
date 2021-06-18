83d7a6532315c8e19a8817750cf1fbc6081f8ad9
dune-official <lukasbaier@outlook.de>
2021-06-18 12:44:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,21 +4,44 @@ socket.onmessage = function(event) {
   processData(event.data);
 };
 
+function toDHMS (unix) {
+  days = {
+    value: Math.floor(unix / 86400),
+    rest: unix % 86400
+  };
+  hours = {
+    value: Math.floor(days.rest / 3600),
+    rest: days.rest % 3600
+  };
+  minutes = {
+    value: Math.floor(hours.rest / 60),
+    rest: hours.rest % 60
+  };
+  seconds = Math.floor(minutes.rest);
+  
+  return {
+    days: days.value,
+    hours: hours.value,
+    minutes: minutes.value,
+    seconds: seconds
+  };
+}
+
 function processData (data) {
   // Gather information about the respective countdown
   let info = JSON.parse(data);
   let time = info.seconds_left;
   let name = info.name;
 
-  let resulting_Date = new Date(time * 1000);
-  let timestring = `das bedeutet ${resulting_Date.getDay()} Tage, ${resulting_Date.getHours()} Stunden, ${resulting_Date.getMinutes()} Minuten und ${resulting_Date.getSeconds()} Sekunden`
+  let resulting_Date = toDHMS(time);
+  let timestring = `das bedeutet ${resulting_Date.days} Tage, ${resulting_Date.hours} Stunden, ${resulting_Date.minutes} Minuten und ${resulting_Date.seconds} Sekunden`
   modifyElements(name, time, timestring);
   
 }
 
 function modifyElements(name, total, timestr) {
   let heading = document.getElementById("headline");
-  heading.textContent = 'Countdown (' + name + ')';
+  heading.textContent = name;
   
   let unixdiv = document.getElementById("unix");
   unixdiv.textContent = total + ' Sekunden,';
@@ -26,4 +49,3 @@ function modifyElements(name, total, timestr) {
   let timestringdiv = document.getElementById("datetime_string");
   timestringdiv.textContent = timestr;
 }
-
